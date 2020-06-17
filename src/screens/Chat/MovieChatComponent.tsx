fa4e574472ce816ab169e074dca372ba0b6a8c2d
Junior Oliveira <arojunior@gmail.com>
2020-06-17 21:17:25 +0200

    add movie chat comment form

--- src/screens/Chat/MovieChatComponent.tsx
+++ src/screens/Chat/MovieChatComponent.tsx
@@ -1,11 +1,62 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
+import React from 'react';
+import {
+  KeyboardAvoidingView,
+  Keyboard,
+  Text,
+  TouchableWithoutFeedback,
+  Platform,
+} from 'react-native';
+import styled from 'styled-components/native';
+import { Input, Button } from 'react-native-elements';
+
+const ContentWrapper = styled.View`
+  flex: 1;
+`;
+
+const FormWrapper = styled.View`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+`;
+
+const FormContent = styled.View`
+  flex: 1;
+  flex-direction: row;
+`;
+
+const InputWrapper = styled.View`
+  width: 70%;
+`;
+
+const ButtonWrapper = styled.View`
+  width: 30%;
+  padding-right: 10px;
+  padding-left: 10px;
+`;
 
 const MovieChatComponent = () => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Chat Screen</Text>
-    </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
+      style={{ flex: 1 }}
+      keyboardVerticalOffset={80}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <ContentWrapper>
+          <Text>Chat Screen</Text>
+          <FormWrapper>
+            <FormContent>
+              <InputWrapper>
+                <Input placeholder="Comment" />
+              </InputWrapper>
+              <ButtonWrapper>
+                <Button title="Submit" />
+              </ButtonWrapper>
+            </FormContent>
+          </FormWrapper>
+        </ContentWrapper>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 };
 
